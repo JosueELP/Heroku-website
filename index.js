fff6f6992a16235e4a2adc4deb683285b95122a1
Josué Luna <josueenrique1999@gmail.com>
2021-05-14 13:43:35 -0500

    Project Setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require("path");
+
+//To set views
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+//To be able to use css and js files
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render("home")
+})
+
+app.listen(port, () => {
+  console.log(`This is running!`)
+})
